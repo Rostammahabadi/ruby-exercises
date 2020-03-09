f913a124759529910e969411d2823017190c2b94
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-09 11:14:10 -0600

    Add dragon, unicorn, and centaur

---
+++ mythical-creatures/lib/centaur.rb
@@ -0,0 +1,87 @@
+class Centaur
+  attr_accessor :name, :breed, :getting_cranky
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+    @cranky = false
+    @standing = true
+    @getting_cranky = 0
+    @shoot = "Twang!!!"
+    @run = "Clop clop clop clop!!!"
+    @sleep = "NO!"
+    @is_sick = false
+  end
+
+  def shoot
+    @getting_cranky += 1
+    if @getting_cranky == 3 || @standing == false
+      @cranky = true
+      @shoot = "NO!"
+      @run = "NO!"
+      @shoot
+    end
+    @shoot
+  end
+
+  def run
+    @getting_cranky += 1
+    if @getting_cranky == 3 || @standing == false
+      @cranky = true
+      @run = "NO!"
+      @shoot = "NO!"
+      @run
+    end
+    @run
+  end
+
+  def cranky?
+    @cranky = true if @getting_cranky == 3
+  end
+
+  def standing?
+    @standing
+  end
+
+  def sleep
+    if @standing == false
+      @cranky = false
+      @sleep = "OK!"
+      @getting_cranky = 0
+      @shoot = "Twang!!!"
+      @run = "Clop clop clop clop!!!"
+    else
+      @sleep
+    end
+  end
+
+  def lay_down
+    @standing=false
+    @standing
+  end
+
+  def laying?
+    return true if @standing == false
+  end
+
+  def stand_up
+    @standing = true
+    @standing
+  end
+
+  def drink_potion
+    if @standing == false
+    @cranky = false
+  elsif @cranky == false
+    @is_sick = true
+    false
+  end
+end
+
+  def rested?
+    true if @cranky == false
+  end
+
+  def is_sick?
+    @is_sick
+  end
+end
