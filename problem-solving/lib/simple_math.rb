e42b61c2c548300ab1174fb28ff8a8dbba64b2cf
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-13 18:11:02 -0600

    Add simple_math

---
+++ problem-solving/lib/simple_math.rb
@@ -0,0 +1,23 @@
+class SimpleMath
+
+  def initialize()
+
+  end
+
+  def add(first_num, second_num)
+    return first_num + second_num
+  end
+
+  def subtract(first_num, second_num)
+    return first_num - second_num
+  end
+
+  def multiply(first_num, second_num)
+    return first_num * second_num
+  end
+
+  def divide(first_num, second_num)
+    return first_num / second_num
+  end
+
+end
--- problem-solving/test/simple_math_test.rb
+++ problem-solving/test/simple_math_test.rb
@@ -1,5 +1,5 @@
 require_relative "test_helper"
-require "./lib/simple_math"
+require_relative "../lib/simple_math"
 
 class SimpleMathTest < Minitest::Test
 
@@ -42,30 +42,48 @@ class SimpleMathTest < Minitest::Test
   def test_it_can_multiply_two_numbers
 
     # instantiate the class
-    #
+    sm = SimpleMath.new
     # call the method being tested
     # determine your expected result
-    #
+    result = sm.multiply(5,10)
+    expected = 50
     # check results
+
+    assert_equal expected, result
   end
 
 
   def test_it_can_multiply_two_other_numbers
 
     # instantiate the class
-    #
+    sm = SimpleMath.new
     # call the method being tested
     # determine your expected result
-    #
+    result = sm.multiply(20,100)
+    expected = 2000
     # check results
+
+    assert_equal expected, result
   end
 
   def test_it_can_divide_two_numbers
     # your code goes here
+    sm = SimpleMath.new
+
+    result = sm.divide(2000,100)
+    expected = 20
+
+    assert_equal expected, result
   end
 
   def test_it_can_divide_two_other_numbers
     # # your code goes here
+    sm = SimpleMath.new
+
+    result = sm.divide(200000000,2)
+    expected = 100000000
+
+    assert_equal expected, result
   end
 
 end
